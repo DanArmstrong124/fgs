d527256ab20c21c7a35e972b87c1f09264e1b3cd
DanArmstrong124 <danielarmstrong124@gmail.com>
2022-06-20 15:35:11 +0000

    mobile

--- script.js
+++ script.js
@@ -9,6 +9,9 @@ var sertext = document.getElementById('servicetext');
 var serone = document.getElementById('serviceone');
 var sertwo = document.getElementById('servicetwo');
 var serthree = document.getElementById('servicethree');
+var soi = document.getElementById('soi');
+var sti = document.getElementById('sti');
+var sthi = document.getElementById('sthi');
 
 setInterval(function(){
     if (scrollY < 249) {
@@ -79,27 +82,39 @@ setInterval(function(){
     if ((scrollY < 2400)||(scrollY > 3201)){
         serone.classList.add('serviceone');
         serone.classList.remove('serviceonem');
+        soi.classList.add('soi');
+        soi.classList.remove('soim');
     }
     if ((scrollY > 2401)&&(scrollY < 3200)){
         serone.classList.remove('serviceone');
         serone.classList.add('serviceonem');
+        soi.classList.remove('soi');
+        soi.classList.add('soim');
     }
 
     if ((scrollY < 3600)||(scrollY > 4401)){
         sertwo.classList.add('servicetwo');
         sertwo.classList.remove('servicetwom');
+        soi.classList.add('sti');
+        soi.classList.remove('stim');
     }
     if ((scrollY > 3601)&&(scrollY < 4400)){
         sertwo.classList.remove('servicetwo');
         sertwo.classList.add('servicetwom');
+        soi.classList.remove('sti');
+        soi.classList.add('stim');
     }
 
     if ((scrollY < 5000)||(scrollY > 5801)){
         serthree.classList.add('servicethree');
         serthree.classList.remove('servicethreem');
+        soi.classList.add('sthi');
+        soi.classList.remove('sthim');
     }
     if ((scrollY > 5001)&&(scrollY < 5800)){
         serthree.classList.remove('servicethree');
         serthree.classList.add('servicethreem');
+        soi.classList.remove('sthi');
+        soi.classList.add('sthim');
     }
 },50);
