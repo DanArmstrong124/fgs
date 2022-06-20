5b34cdf9c56041c680f9b7292bbea6f40bddf00b
DanArmstrong124 <danielarmstrong124@gmail.com>
2022-06-20 10:59:32 +0000

    tes

--- script.js
+++ script.js
@@ -5,11 +5,43 @@ var th = document.getElementById('titleh');
 var ts = document.getElementById('titles');
 
 setInterval(function(){
-    if (scrollY > 60){
+    if (scrollY < 60) {
+        nav.classList.add('nav');
+        ft.classList.add('fulltitle');
+        tf.classList.add('titlef');
+        th.classList.add('titleh');
+        ts.classList.add('titles');
+
+        nav.classList.remove('navm');
+        ft.classList.remove('fulltitlem');
+        tf.classList.remove('titlefm');
+        th.classList.remove('titlehm');
+        ts.classList.remove('titlesm');
+    }
+    if (scrollY > 60 < 120){
+        nav.classList.remove('nav');
+        ft.classList.remove('fulltitle');
+        tf.classList.remove('titlef');
+        th.classList.remove('titleh');
+        ts.classList.remove('titles');
+
+        tf.classList.remove('titlefmt');
+        th.classList.remove('titlehmt');
+        ts.classList.remove('titlesmt');
+
         nav.classList.add('navm');
         ft.classList.add('fulltitlem');
         tf.classList.add('titlefm');
         th.classList.add('titlehm');
         ts.classList.add('titlesm');
+    }
+    if (scrollY > 120){
+        tf.classList.add('titlefmt');
+        th.classList.add('titlehmt');
+        ts.classList.add('titlesmt');
+
+        tf.classList.remove('titlefm');
+        th.classList.remove('titlehm');
+        ts.classList.remove('titlesm');
     }
 },50);
